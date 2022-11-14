73b69253ff287efc0132d54a042716b200ede2ef
torasmu <tomras12@gmail.com>
2022-11-14 14:26:37 -0500

    Add prettier

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,22 +1,21 @@
-import { App, GitHubSourceCodeProvider } from '@aws-cdk/aws-amplify-alpha';
-import { SecretValue, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { App, GitHubSourceCodeProvider } from '@aws-cdk/aws-amplify-alpha'
+import { SecretValue, Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
 
 export class PersonalSiteInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-
-    const amplifyApp = new App(this, "personal-site", {
+    const amplifyApp = new App(this, 'personal-site', {
       sourceCodeProvider: new GitHubSourceCodeProvider({
-        owner: "torasmu",
-        repository: "me",
+        owner: 'torasmu',
+        repository: 'me',
         // Had to create this secret manually in the console
-        oauthToken: SecretValue.secretsManager("github-aws-amplify-token", {
-          jsonField: "token",
+        oauthToken: SecretValue.secretsManager('github-aws-amplify-token', {
+          jsonField: 'token',
         }),
-      })
-    });
-    const mainBranch = amplifyApp.addBranch("main");
+      }),
+    })
+    const mainBranch = amplifyApp.addBranch('main')
   }
 }
