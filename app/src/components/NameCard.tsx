38b9e092a90ce8382970cdd02464ce988ef0f23b
torasmu <tomras12@gmail.com>
2023-02-08 21:27:52 -0500

    use digital portrait

--- app/src/components/NameCard.tsx
+++ app/src/components/NameCard.tsx
@@ -15,13 +15,14 @@ const Header = styled.div`
 `
 
 const Headshot = styled.img`
-  width: 200px;
-  height: 200px;
+  width: 240px;
+  height: 240px;
   margin: 30px;
   transform: rotate(5deg) skew(5deg);
   // tan(5 deg) == 8.7%
   -webkit-clip-path: polygon(0 8.7%, 0 100%, 100% 91.3%, 100% 0);
   clip-path: polygon(0 8.7%, 0 100%, 100% 91.3%, 100% 0);
+  background-color: #1220ba;
 `
 
 type NameCardProps = {
@@ -35,7 +36,7 @@ const NameCard = ({ name, job, location }: NameCardProps) => {
     <Card>
       <Header>tomras.dev</Header>
       <div>
-        <Headshot src="/headshot.jpeg" alt="Headshot" />
+        <Headshot src="/digital_portrait_800x800.png" alt="Headshot" />
         <PersonData type={PersonDataType.Name} text={name} />
         <PersonData type={PersonDataType.Job} text={job} />
         <PersonData type={PersonDataType.Location} text={location} />
